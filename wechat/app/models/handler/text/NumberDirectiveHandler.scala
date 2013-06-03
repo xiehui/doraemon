019f3f189e787d56f55b35abe10f20ebfa747e2c
unknown <xiehui@G-040101-045.tuniu-inc.com>
2013-06-03 18:14:05 +0800

    design

--- wechat/app/models/handler/text/NumberDirectiveHandler.scala
+++ wechat/app/models/handler/text/NumberDirectiveHandler.scala
@@ -1,10 +1,11 @@
 package models.handler.text
 
 import models.ReplyMsg
+import models.ReceivedTextMsg
 
 class NumberDirectiveHandler extends DirectiveHandler {
 
-  override def hand(directive: String, param: String) : ReplyMsg = {
+  override def hand(directive: String, param: String, msg: ReceivedTextMsg) : ReplyMsg = {
     null
   }
 }
